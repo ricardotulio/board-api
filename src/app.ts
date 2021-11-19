3002398364a96c51f2f0269173d8042af66b2229
Ricardo Tulio <ledo.tulio@gmail.com>
2021-11-19 21:11:14 +0000

    http: create /board/:id route

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,13 @@ import container from './infraestructure/container'
 const port = process.env.SERVER_PORT || 8000
 const app = express()
 
+app.get('/board/:id', (request, response) => {
+  const id = request.params.id
+  const GetBoardController = container.get('GetBoardController')
+
+  response.send(GetBoardController(id))
+})
+
 app.listen(port, () => {
   console.log(`running on ${port}`)
 })
