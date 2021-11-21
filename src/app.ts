08fc8ff637fa7e39e2b22d5cd021d99a33509952
Ricardo Tulio <ledo.tulio@gmail.com>
2021-11-21 20:01:37 +0000

    fix: set express to receive json requests

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import routes from './infraestructure/http/routes'
 const port = process.env.SERVER_PORT || 8000
 const app = express()
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(routes)
 
 app.listen(port, () => {
