7606b358506c2d4bbf43983363a6a8c9b173b653
Ricardo Tulio <ledo.tulio@gmail.com>
2021-11-21 20:03:43 +0000

    repositories: update IBoardRepository interface to return a Board after presist

--- src/domain/repositories/IBoardRepository.ts
+++ src/domain/repositories/IBoardRepository.ts
@@ -1,9 +1,9 @@
-import { IBoard } from "../entities/IBoard";
+import IBoard from '../entities/IBoard'
 
 interface IBoardRepository {
   get(id: string): IBoard,
   getList(): Array<IBoard>,
-  persist(board: IBoard): void
+  persist(board: IBoard): IBoard
 }
 
 export default IBoardRepository
